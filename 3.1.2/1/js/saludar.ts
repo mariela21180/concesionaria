abab712fa297ea4208997f2a1343473e9cb632c2
Mariela Gonzalez <mariela21180@gmail.com>
2019-07-09 23:42:52 -0300

    3.1.2 - Correcciones ejercicio en clase 1

--- 3.1.2/1/js/saludar.ts
+++ 3.1.2/1/js/saludar.ts
@@ -1,5 +1,5 @@
 import * as readlineSync from '../node_modules/readline-sync'; 
 
-let nombre = readlineSync.question("Ingrese su nombre completo: ");
+let nombre: string = readlineSync.question("Ingrese su nombre completo: ");
 
 console.log("Hola " + nombre);
