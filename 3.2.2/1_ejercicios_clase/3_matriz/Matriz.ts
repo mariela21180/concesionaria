1b7ad1337b8bbebbf9a7b5cf4026808a6e3155af
Mariela Gonzalez <mariela21180@gmail.com>
2019-07-22 19:28:57 -0300

    3.2.2 - Ejercicios en clase - Clase matriz

---
+++ 3.2.2/1_ejercicios_clase/3_matriz/Matriz.ts
@@ -0,0 +1,25 @@
+export default class Matriz {
+    private arreglo: string[][];
+
+    public constructor() {
+        this.arreglo = new Array();        
+    }
+
+    public cargarMatriz(columna1: string, columna2:string): void {
+        let aux: string[] = [
+            columna1,
+            columna2
+        ];
+        this.arreglo.push(aux);
+    }
+
+    public get(x: number, y:number): string {
+        let filas: number = this.arreglo.length;
+        let columnas: number = this.arreglo[0].length;
+        if ( ( filas >= x) && ( columnas >= y )){
+            return this.arreglo[x][y];
+        } else {
+            return "Los valores ingresados se encuentran fuera del rango de la matriz";
+        }
+    }
+}
