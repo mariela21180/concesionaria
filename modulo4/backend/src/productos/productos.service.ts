09adea3318c7b796e307f9ceea3b2add1053b455
Mariela Gonzalez <mariela21180@gmail.com>
2019-08-28 21:04:36 -0300

    4.2.2 - Ejercicio 2 - Poniendo arreglo de productos en el servicio

--- modulo4/backend/src/productos/productos.service.ts
+++ modulo4/backend/src/productos/productos.service.ts
@@ -2,17 +2,50 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class ProductosService {
+    private productos = [{
+        "producto": "Papa",
+        "precio": 50,
+        "descripcion": "-"
+    }, {
+        "producto": "Pera",
+        "precio": 70,
+        "descripcion": "-"
+    }, {
+        "producto": "Cebolla",
+        "precio": 40,
+        "descripcion": "-"
+    }, {
+        "producto": "Tomate",
+        "precio": 100,
+        "descripcion": "-"
+    }, {
+        "producto": "Morron",
+        "precio": 200,
+        "descripcion": "-"
+    }, {
+        "producto": "Arroz",
+        "precio": 35,
+        "descripcion": "-"
+    }, {
+        "producto": "Aceite",
+        "precio": 60,
+        "descripcion": "-"
+    }]
     private static readonly CANTIDAD_PRODUCTOS = 10;
     public getProducto(): any {
-        let productos = [];
-        for (let i = 0; i < ProductosService.CANTIDAD_PRODUCTOS; i++) {
-            let producto = {
-                'producto': 'producto_' + i,
-                'precio': Math.floor(Math.random() * 100),
-                'descripcion': 'descripcion_' + i
-            };
-            productos.push(producto);
-        }
-        return productos;
+        // let productos = [];
+        // for (let i = 0; i < ProductosService.CANTIDAD_PRODUCTOS; i++) {
+        //     let producto = {
+        //         'producto': 'producto_' + i,
+        //         'precio': Math.floor(Math.random() * 100),
+        //         'descripcion': 'descripcion_' + i
+        //     };
+        //     productos.push(producto);
+        // }
+        // return productos;
+
+
+        
+        return this.productos;
     }
 }
