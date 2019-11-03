78d42fdb3ecd3ef79921c28c37d24b4480ad88e3
Mariela Gonzalez <mariela21180@gmail.com>
2019-11-03 17:20:59 -0300

    2.2

--- modulo5/5.1.11/2.2 - SQL con jerarquia recursivo.sql
+++ modulo5/5.1.11/2.2 - SQL con jerarquia recursivo.sql
@@ -1,12 +1,12 @@
-drop schema arbol;
-
+drop schema if exists arbol;
 create schema arbol;
-
 use arbol;
 
+drop table if exists Entidad;
 create table if not exists Entidad (
 id_identidad int,
 Entidad_descripcion varchar(45),
 es_hoja boolean,
-prima
-)
+primary key (id_identidad)
+);
+
