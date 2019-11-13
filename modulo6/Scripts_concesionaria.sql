1f1277ad4a75ad5d8a6c36ab8c28a66a1620c1f9
Mariela Gonzalez <mariela21180@gmail.com>
2019-11-13 10:03:55 -0300

    Eliminando archivos del otro repo

--- modulo6/Scripts_concesionaria.sql
+++
@@ -1,7 +0,0 @@
-use concesionaria;
-
-insert into vehiculo values(1,"auto","Renault","12",1980,50000,250000,534,"UST182",4,null,true);
-insert into vehiculo values(2,"camioneta","Peugeot","Partner",2018,250000,10000,4087,"AKK143",5,2,true);
-insert into vehiculo values(3,"camioneta","Fiat","Fiorino",2019,350000,8000,5087,"AZZ123",4,2,true);
-insert into vehiculo values(4,"auto","Peugeot","406",2018,10000,250000,4087,"AKZ543",5,2,true);
-insert into vehiculo values(5,"auto","Fiat","Palio",2014,250000,30000,2500,"OAO321",5,2,true);
